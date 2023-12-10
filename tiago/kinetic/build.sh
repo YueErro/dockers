31953aa29f7d81caf17181c885bf859f2f0555f5
YueErro <yue.trbj@gmail.com>
2023-12-10 13:49:32 +0100

    update Dockerfiles and remove deprecated ones

--- tiago/kinetic/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-docker build --build-arg IMAGE_NAME="tiago-kinetic" -t tiago-kinetic .
--- tiago/melodic/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-docker build --build-arg IMAGE_NAME="tiago-melodic" -t tiago-melodic .
